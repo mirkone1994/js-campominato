ce650b13a670eb5c9571eef78bf85893d7833b72
mirkone1994 <mirkochiappori@gmail.com>
2021-07-14 18:23:16 +0200

    added score on html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var numbers = [];
 var numbersUser = [];
 var score = 0;
+var scoreElement = document.getElementById("score");
 while (numbers.length < 16) {
     var sixteen = Math.floor(Math.random() * 100 ) + 1;
     if(numbers.indexOf(sixteen) === -1) {
@@ -23,3 +24,5 @@ while (i < numbers.length) {
     console.log(score);
     i++;
 }
+scoreElement.innerHTML = "Il tuo punteggio finale è: " + score; 
+
