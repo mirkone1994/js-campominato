179f2e35662aa64e6b0aa54b916643600fbc0ad4
mirkone1994 <mirkochiappori@gmail.com>
2021-07-14 17:34:57 +0200

    user numbers choice

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 var numbers = [];
-for (i = 0; i < 16; i++) {
-    numbers.push(Math.floor(Math.random() * 100 ) + 1)
+var numbersUser = [];
+while (numbers.length < 16) {
+    var sixteen = Math.floor(Math.random() * 100 ) + 1;
+    if(numbers.indexOf(sixteen) === -1) {
+        numbers.push(sixteen);
+    };
 }
 console.log(numbers);
+var i = 0
+while (i < numbers.length) {
+    var askNumber = parseInt(prompt("Inserisci un numero da 1 a 100"));
+    numbersUser.push(askNumber);
+    i++;
+}
+console.log(numbersUser);
